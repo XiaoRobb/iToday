21eba3e3897af1fb340a43a4416a4e44bc81ce61
Long46458 <43133907+Long46458@users.noreply.github.com>
2018-12-30 23:48:51 +0800

    王文龙的提交（历史上的今天部分）

---
+++ TodayHistory/TodayHistory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iToday
+{
+    public class TodayHistory
+    {
+        public string HistoryThing { set; get; }                //今日热点事件文字说明
+        public string ImageUrl { set; get; }                //今日热点事件配图链接
+        public string Url { set; get; }                     //今日热点事件链接
+
+        public TodayHistory(string hotThing, string image, string url)     //构造函数
+        {
+            HistoryThing = hotThing;
+            ImageUrl = image;
+            Url = url;
+        }
+    }
+}
